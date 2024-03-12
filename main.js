7c2fd611efc88eaefd477624bfb3468fbd1eff79
ronu34 <w24133296@gmail.com>
2024-03-12 16:38:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function draw() {}
+
+function clearcanvas() {
+    background("white");
+    console.log("Canvas : Cleared");
+}
